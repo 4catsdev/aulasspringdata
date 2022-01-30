c0e19db06d36f97c80acdd0b442e6814e32c4be8
4CatsDev <4catsdev@gmail.com>
2022-01-29 22:25:36 -0400

    Criando o projeto

---
+++ src/main/java/com/fourcatsdev/aulasspringdata/AulasspringdataApplication.java
@@ -0,0 +1,13 @@
+package com.fourcatsdev.aulasspringdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasspringdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasspringdataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fourcatsdev/aulasspringdata/AulasspringdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourcatsdev.aulasspringdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasspringdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
